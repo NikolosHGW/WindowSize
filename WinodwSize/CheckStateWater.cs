c532780efaf6cf5e07e994c19d7bad24d0ea3557
Ник Николос <nic23172@gmail.com>
2020-05-27 18:08:51 +0300

    in CheckStateWater added ChckLine

--- WinodwSize/CheckStateWater.cs
+++ WinodwSize/CheckStateWater.cs
@@ -25,8 +25,9 @@ namespace WinodwSize
                 FillModel.FillingAsync(600, DataLines.wnd.line3, DataLines.Y2, DataLines.anLine3);
             }
 
-            if (StateButton.ButtonV[12].IsState == true && DataLines.wnd.line3.Opacity == 1 && DataLines.wnd.line5.Opacity == 0)
+            if (StateButton.ButtonV[12].IsState == true && DataLines.wnd.line3.Opacity == 1 && DataLines.wnd.line5.Opacity == 0 && DataLines.ChckLine3 == true)
             {
+                DataLines.ChckLine3 = false;
                 FillModel.Filling(DataLines.wnd.line5, DataLines.Y2, DataLines.anLine5);
                 FillModel.FillingAsync(300, DataLines.wnd.line7, DataLines.X2, DataLines.anLine7);
                 FillModel.FillingAsync(300, DataLines.wnd.line9, DataLines.X2, DataLines.anLine9);
@@ -35,13 +36,15 @@ namespace WinodwSize
 
 
 
-            if (StateButton.Button[7].IsState == true && DataLines.wnd.line7.Opacity == 1 && DataLines.wnd.line13.Opacity == 0)
+            if (StateButton.Button[7].IsState == true && DataLines.wnd.line7.Opacity == 1 && DataLines.wnd.line13.Opacity == 0 && DataLines.ChckLine7 == true)
             {
+                DataLines.ChckLine7 = false;
                 FillModel.Filling(DataLines.wnd.line13, DataLines.X2, DataLines.anLine13);
                 FillModel.FillingAsync(300, DataLines.wnd.line16, DataLines.Y2, DataLines.anLine16);
             }
-            if (StateButton.Button[7].IsState == true && DataLines.wnd.line14.Opacity == 1 && DataLines.wnd.line8.Opacity == 0)
+            if (StateButton.Button[7].IsState == true && DataLines.wnd.line14.Opacity == 1 && DataLines.wnd.line8.Opacity == 0 && DataLines.ChckLine14 == true)
             {
+                DataLines.ChckLine14 = false;
                 FillModel.Filling(DataLines.wnd.line8, DataLines.X2, DataLines.anLine8);
                 FillModel.FillingAsync(300, DataLines.wnd.line6, DataLines.Y2, DataLines.anLine6);
                 FillModel.FillingAsync(300, DataLines.wnd.line9, DataLines.X2, DataLines.anLine9);
@@ -52,28 +55,32 @@ namespace WinodwSize
 
 
 
-            if (StateButton.Button[6].IsState == true && DataLines.wnd.line13.Opacity == 1 && DataLines.wnd.line17.Opacity == 0)
+            if (StateButton.Button[6].IsState == true && DataLines.wnd.line13.Opacity == 1 && DataLines.wnd.line17.Opacity == 0 && DataLines.ChckLine13 == true)
             {
+                DataLines.ChckLine13 = false;
                 FillModel.Filling(DataLines.wnd.line17, DataLines.X2, DataLines.anLine17);
                 FillModel.FillingAsync(100, DataLines.wnd.line20, DataLines.Y2, DataLines.anLine20);
                 FillModel.FillingAsync(280, DataLines.wnd.line22, DataLines.Y2, DataLines.anLine22);
             }
-            if (StateButton.Button[6].IsState == true && DataLines.wnd.line18.Opacity == 1 && DataLines.wnd.line14.Opacity == 0)
+            if (StateButton.Button[6].IsState == true && DataLines.wnd.line18.Opacity == 1 && DataLines.wnd.line14.Opacity == 0 && DataLines.ChckLine18 == true)
             {
+                DataLines.ChckLine18 = false;
                 FillModel.Filling(DataLines.wnd.line14, DataLines.X2, DataLines.anLine14);
                 FillModel.FillingAsync(100, DataLines.wnd.line16, DataLines.Y2, DataLines.anLine16);
             }
 
 
 
-            if (StateButton.Button[5].IsState == true && DataLines.wnd.line17.Opacity == 1 && DataLines.wnd.line25.Opacity == 0)
+            if (StateButton.Button[5].IsState == true && DataLines.wnd.line17.Opacity == 1 && DataLines.wnd.line25.Opacity == 0 && DataLines.ChckLine17 == true)
             {
+                DataLines.ChckLine17 = false;
                 FillModel.Filling(DataLines.wnd.line25, DataLines.X2, DataLines.anLine25);
                 FillModel.Filling(DataLines.wnd.line24, DataLines.Y2, DataLines.anLine24);
                 FillModel.FillingAsync(300, DataLines.wnd.line28, DataLines.Y2, DataLines.anLine28);
             }
-            if (StateButton.Button[5].IsState == true && DataLines.wnd.line26.Opacity == 1 && DataLines.wnd.line18.Opacity == 0)
+            if (StateButton.Button[5].IsState == true && DataLines.wnd.line26.Opacity == 1 && DataLines.wnd.line18.Opacity == 0 && DataLines.ChckLine26 == true)
             {
+                DataLines.ChckLine26 = false;
                 FillModel.Filling(DataLines.wnd.line18, DataLines.X2, DataLines.anLine18);
                 FillModel.Filling(DataLines.wnd.line22, DataLines.Y2, DataLines.anLine22);
                 FillModel.FillingAsync(280, DataLines.wnd.line20, DataLines.Y2, DataLines.anLine20);
